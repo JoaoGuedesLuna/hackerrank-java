5252ba3b62debbfc8191b96594ca91886f93c2f8
JoaoGuedesLuna <joaoguedesluna@gmail.com>
2026-03-12 18:12:23 -0300

    :sparkles: feat: add solution for challenge 'Welcome to Java!'

---
+++ src/main/java/dev/guedes/hackerrank/introduction/easy/WelcomeToJava.java
@@ -0,0 +1,39 @@
+package main.java.dev.guedes.hackerrank.introduction.easy;
+
+/**
+ * Welcome to the world of Java! In this challenge, we practice printing to stdout.
+ * <br><br>
+ * The code stubs in your editor declare a Solution class and a main method. Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
+ * <br><br>
+ * <code>
+ *     System.out.println("Hello, World.");<br>
+ *     System.out.println("Hello, Java.");
+ * </code>
+ * <br><br>
+ * <strong>Input Format</strong>
+ * <br><br>
+ * There is no input for this challenge.
+ * <br><br>
+ * <strong>Output Format</strong>
+ * <br><br>
+ * You must print two lines of output:
+ * <ol>
+ *     <li>Print <code>Hello, World.</code> on the first line.</li>
+ *     <li>Print <code>Hello, Java.</code> on the second line.</li>
+ * </ol>
+ * <br><br>
+ * <strong>Sample Output</strong>
+ * <br><br>
+ * <code>
+ *     Hello, World.<br>
+ *     Hello, Java.
+ * </code>
+ *
+ * @author João Guedes
+ */
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
